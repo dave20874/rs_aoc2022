c8ec0b2ba2c4a85da0d8994eb0e73c65b7849753
David Wheeler <dwheeler20874@gmail.com>
2022-12-08 00:06:32 -0500

    Removed obsolete code from Day6.

--- src/day6.rs
+++ src/day6.rs
@@ -26,68 +26,6 @@ impl Day6 {
         Day6 { seq }
     }
 
-    fn find_marker(&self) -> usize {
-        let mut marker: [char; 4] = ['x', 'x', 'x', 'x'];
-
-        marker[0] = self.seq[0];
-        marker[1] = self.seq[1];
-        marker[2] = self.seq[2];
-        let mut replace = 3;
-        let mut index = 3;
-        let mut found = false;
-
-        while !found {
-            // put next char in the marker buffer
-            marker[replace] = self.seq[index];
-            replace = (replace + 1) % marker.len();
-            index += 1;
-
-            // check to see if all marker values are unique
-            found = true;
-            for i in 0..3 {
-                for j in i+1..4 {
-                    if marker[i] == marker[j] {
-                        found = false;
-                    }
-                }
-            }
-        }
-
-        index
-    }
-
-    fn find_start(&self) -> usize {
-        let mut marker: VecDeque<char> = VecDeque::new();
-
-        for i in 0..13 {
-            marker.push_back(self.seq[i]);
-        }
-
-        let mut index = 13;
-        let mut found = false;
-
-        while !found {
-            // put next char in the marker buffer
-            marker.push_back(self.seq[index]);
-            index += 1;
-
-            // check to see if all marker values are unique
-            found = true;
-            for i in 0..marker.len()-1 {
-                for j in i+1..marker.len() {
-                    if marker[i] == marker[j] {
-                        found = false;
-                    }
-                }
-            }
-
-            // remove oldest from marker
-            marker.pop_front();
-        }
-
-        index
-    }
-
     fn find_no_repeat(&self, len:usize) -> usize {
         let mut marker: VecDeque<char> = VecDeque::new();
 
@@ -143,43 +81,6 @@ mod tests {
     fn test_load() {
         let d = Day6::load("examples/day6_example1.txt");
         assert_eq!(d.seq.len(), 30);
-    }
-
-    #[test]
-    fn test_find_marker() {
-        let cases: Vec<(&str, usize)> = vec!(
-            ("examples/day6_example1.txt", 7),
-            ("examples/day6_example2.txt", 5),  
-            ("examples/day6_example3.txt", 6),  
-            ("examples/day6_example4.txt", 10),  
-            ("examples/day6_example5.txt", 11),  
-        );
-
-        for (filename, expected) in cases {
-            let d = Day6::load(filename);
-            let marker = d.find_marker();
-            assert_eq!(marker, expected)
-        }
-
-    }
-
-
-    #[test]
-    fn test_find_start() {
-        let cases: Vec<(&str, usize)> = vec!(
-            ("examples/day6_example1.txt", 19),
-            ("examples/day6_example2.txt", 23),  
-            ("examples/day6_example3.txt", 23),  
-            ("examples/day6_example4.txt", 29),  
-            ("examples/day6_example5.txt", 26),  
-        );
-
-        for (filename, expected) in cases {
-            let d = Day6::load(filename);
-            let marker = d.find_start();
-            assert_eq!(marker, expected)
-        }
-
     }
 
     #[test]
