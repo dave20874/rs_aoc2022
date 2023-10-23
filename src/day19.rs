8450d1045e9b2184baa2bab759d1309dd3b37121
David Wheeler <dwheeler20874@gmail.com>
2023-10-23 13:06:54 -0400

    Not working yet.

--- src/day19.rs
+++ src/day19.rs
@@ -6,7 +6,7 @@ use lazy_static::lazy_static;
 use regex::Regex;
 use priority_queue::PriorityQueue;
 
-#[derive(Hash, std::cmp::PartialEq, std::cmp::Eq, Clone)]
+#[derive(Hash, std::cmp::PartialEq, std::cmp::Eq, Clone, Debug)]
 enum Action {
     Start_Ore,
     Start_Clay,
@@ -14,30 +14,46 @@ enum Action {
     Start_Geode,
 }
 
-const PART1_ACTIONS: [Action; 3] = [Action::Start_Clay, Action::Start_Obsidian, Action::Start_Geode];
+const PART1_ACTIONS: [Action; 4] = [Action::Start_Ore, Action::Start_Clay, Action::Start_Obsidian, Action::Start_Geode];
 
+#[derive(Clone, Hash, PartialEq, Eq, Debug)]
 struct ActionList {
-    action: Vec<(t, Action, count)>
+    action: Vec<(usize, Action, usize)>  // time, action, count
 }
+
 
 impl ActionList {
     pub fn new() -> ActionList {
-        ActionList { action: vec::new() }
+        ActionList { action: Vec::new() }
     }
 
-    pub fn extend(&self, t: usize, action: Action) -> Option<ActionList> {
+    pub fn extend(&self, t: usize, action: Action) -> ActionList {
         // Make a new action list cloned from this one.
+        let mut newlist: ActionList = self.clone();
 
-        // Check that the time isn't less than the last time already listed.
-        // Check that if the time is equal to the last time listed, the new
-        // action type is the same or a greater type.
+        // If the existing list ends with an action of the same type at the same time,
+        // just add one to that.
+        let len = newlist.action.len();
+        println!("Extending {:?} with {:?} at time {:?}", newlist, action, t);
 
-        // Add the new action at the end
-        // If the last action is the same type, add one to its count.
-        // Otherwise, append a new entry to the vector.
+        if (len >= 1) &&
+           (newlist.action[len-1].0 == t) &&
+           (newlist.action[len-1].1 == action) {
+            // Just increment the last element
+            newlist.action[len-1].2 += 1;
+            println!("  Incremented last element.");
+        }
+        else {
+            // Append a new element to the list
+            newlist.action.push( (t, action, 1));
+            println!("  Pushed new element.");
+        }
+
+        newlist
     }
 }
 
+/*
 struct SimState {
     ore_bots: usize,
     clay_bots: usize,
@@ -74,226 +90,199 @@ impl SimState {
         }
     }
 }
+*/
 
-struct Sim {
-    blueprint: &Blueprint,
-    // TODO
+struct Sim<'a> {
+    blueprint: &'a Blueprint,
+
+    geodes: usize,
+    next_ore_bot: Option<usize>,
+    next_clay_bot: Option<usize>,
+    next_obsidian_bot: Option<usize>,
+    next_geode_bot: Option<usize>,
 }
 
-impl Sim {
-    pub fn new(blueprint: &Blueprint) -> Sim {
-        Sim {blueprint: blueprint}
+impl<'a> Sim<'a> {
+    pub fn new(blueprint: &'a Blueprint) -> Sim {
+        Sim {
+            blueprint: blueprint,
+            geodes: 0,
+            next_ore_bot: None,
+            next_clay_bot: None,
+            next_obsidian_bot: None,
+            next_geode_bot: None,
+        }
     }
 
-    pub fn reset(&self) {
+    fn update_affordability(&mut self, 
+            t: usize, 
+            ore_bots_ordered: usize,
+            ore: usize, 
+            clay_bots_ordered: usize, 
+            clay: usize, 
+            obsidian_bots_ordered: usize, 
+            obsidian: usize) {
 
+        // Check whether the various options are possible at this point.
+        if (ore >= self.blueprint.ore_cost_ore) {
+            // We could add an ore bot at this time
+            if self.next_ore_bot == None {
+                // We couldn't afford one before, so this is the earliest we could add it
+                self.next_ore_bot = Some(t)
+            }
+        }
+        else {
+            // We can't add an ore bot at this time
+            self.next_ore_bot = None;
+        };
+
+        if (ore >= self.blueprint.clay_cost_ore) &&
+           (ore_bots_ordered == 0) {
+            // We could afford a clay bot now
+            if self.next_clay_bot == None {
+                // We couldn't afford one before
+                self.next_clay_bot = Some(t);
+            }
+        }
+        else {
+            // We can't afford a clay bot at this point
+            self.next_clay_bot = None
+        };
+
+        if (ore >= self.blueprint.obsidian_cost_ore) && 
+           (clay >= self.blueprint.obsidian_cost_clay) &&
+           (ore_bots_ordered == 0) &&
+           (clay_bots_ordered == 0) {
+            // We could afford an obsidian bot now
+            if self.next_obsidian_bot == None {
+                // We couldn't afford one before
+                self.next_obsidian_bot = Some(t);
+            }
+        }
+        else {
+            // We can't afford an obsidian bot at this point
+            self.next_obsidian_bot = None;
+        };
+
+        if (ore >= self.blueprint.geode_cost_ore) && 
+           (obsidian >= self.blueprint.geode_cost_obsidian) &&
+           (ore_bots_ordered == 0) &&
+           (clay_bots_ordered == 0) &&
+           (obsidian_bots_ordered == 0) {
+            // We could afford a geode bot now
+            if self.next_geode_bot == None {
+                // We couldn't afford one before
+                Some(t);
+            }
+        }
+        else {
+            // We can't afford a geode bot at this point
+            self.next_geode_bot = None;
+        };
     }
 
-    pub fn run(&self) {
-        // TODO
-    }
+    // run the actionlist and return the number of geodes produced.
+    // returns (geodes, score)
+    pub fn run(&mut self, action_list: &ActionList) -> (usize, usize) {
 
-    pub fn next_action_time(&self, action: &Action);
-}
+        let mut ore = 0;
+        let mut clay = 0;
+        let mut obsidian = 0;
+        let mut geodes = 0;
 
-#[derive(Hash, std::cmp::PartialEq, std::cmp::Eq, Clone)]
-struct Solution {
-    // Each element is a time and action
-    steps: Vec<(usize, Action)>,
-}
+        let mut ore_bots = 1;
+        let mut clay_bots = 0;
+        let mut obsidian_bots = 0;
+        let mut geode_bots = 0;
 
-impl Solution {
-    pub fn new() -> Solution {
-        Solution {time: 0, steps: Vec::new(), geodes: 3 }
-    }
+        let mut ore_bots_ordered = 0;
+        let mut clay_bots_ordered = 0;
+        let mut obsidian_bots_ordered = 0;
+        let mut geode_bots_ordered = 0;
 
-    // Create a new solution that extends another one by one action.
-    fn extend(&self, action: &Action, time: usize) -> Solution {
-        // TODO
-    }
-
-    /*
-    fn action_time(&self, blueprint: Blueprint, action: &Action) -> Option<usize> {
-        let mut retval = None;
-
-        // Simulate the current sequence.
-        // At or after the allowed time, if there are resources to perform the action
-        // that's the time we want to return.
-        let mut state = SimState::new();
-        let mut time = 0;
+        let mut t = 0;
         let mut action_index = 0;
-        let mut ordered_ore_bots = 0;
-        let mut ordered_clay_bots = 0;
-        let mut ordered_obsidian_bots = 0;
-        let mut ordered_geode_bots = 0;
-        let mut outranked = false;
-        let mut affordable = false;
 
-        while (time < TIME_PART1) && 
-              (action_index < self.steps.len()) {}
+        while t < TIME_PART1 {
+            let mut acted = false;
+            if action_index < action_list.action.len() {
+                let (action_time, action, quantity) = &action_list.action[action_index];
+                if t == *action_time {
+                    // Do an action.  Consume resources, order machines for next time step.
+                    match action {
+                        Action::Start_Ore => {
+                            ore -= self.blueprint.ore_cost_ore * quantity;
+                            ore_bots_ordered += quantity;
+                        }
+                        Action::Start_Clay => { 
+                            ore -= self.blueprint.clay_cost_ore * quantity;
+                            clay_bots_ordered += quantity;
+                            self.next_ore_bot = None
+                        }
+                        Action::Start_Obsidian => { 
+                            ore -= self.blueprint.obsidian_cost_ore * quantity;
+                            clay -= self.blueprint.obsidian_cost_clay * quantity;
+                            obsidian_bots_ordered += 1;
+                            self.next_ore_bot = None;
+                            self.next_clay_bot = None;
+                        }
+                        Action::Start_Geode => { 
+                            ore -= self.blueprint.geode_cost_ore * quantity;
+                            obsidian -= self.blueprint.geode_cost_obsidian * quantity;
+                            geode_bots_ordered += 1;
+                            self.next_ore_bot = None;
+                            self.next_clay_bot = None;
+                            self.next_obsidian_bot = None;
+                        }
+                    }
 
-            let (t_step, a_step) = self.steps[action_index];
-
-            // If we are at the time for this action, consume resources
-            // to order bots
-            if time == t_step {
-                match a_step {
-                    Action::Start_Ore(count) => {
-                        state.ore -= count * blueprint.ore_cost_ore;
-                        ordered_ore_bots += count;
-                    }
-                    Action::Start_Clay(count) => {
-                        state.ore -= count * blueprint.clay_cost_ore;
-                        ordered_clay_bots += count;
-                    }
-                    Action::Start_Obsidian(count) => {
-                        state.ore -= count * blueprint.obsidian_cost_ore;
-                        state.clay -= count * blueprint.obsidian_cost_clay;
-                        ordered_clay_bots += count;
-                    }
-                    Action::Start_Geode(count) => {
-                        state.ore -= count * blueprint.geode_cost_ore;
-                        state.obsidian -= count * blueprint.geode_cost_obsidian;
-                        ordered_geode_bots += count;
-                    }
+                    acted = true;
+                    action_index += 1;
                 }
-                // None
-                action_index += 1;
             }
-            
-            // Advance to the next time step
-            if time < t_step {
-                state.ore += state.ore_bots;
-                state.clay += state.clay_bots;
-                state.obsidian += state.obsidian_bots;
-                state.geodes += state.geode_bots;
 
-                state.ore_bots += ordered_ore_bots;
-                ordered_ore_bots = 0;
-                state.clay_bots += ordered_clay_bots;
-                ordered_clay_bots = 0;
-                state.obsidian_bots += ordered_obsidian_bots;
-                ordered_obsidian_bots = 0;
-                state.geode_bots += ordered_geode_bots;
-                ordered_geode_bots = 0;
+            if !acted {
+                // No more actions for this time tick.
+                self.update_affordability(t, ore_bots_ordered, ore, clay_bots_ordered, clay, obsidian_bots_ordered, obsidian);
 
-                time += 1;
+                // Let machines generate new resources.
+                ore += ore_bots;
+                clay += clay_bots;
+                obsidian += obsidian_bots;
+                geodes += geode_bots;
+
+                // Newly ordered machines can come online now.
+                ore_bots += ore_bots_ordered;
+                clay_bots += clay_bots_ordered;
+                obsidian_bots += obsidian_bots_ordered;
+                geode_bots += geode_bots_ordered;
+
+                ore_bots_ordered = 0;
+                clay_bots_ordered = 0;
+                obsidian_bots_ordered = 0;
+                geode_bots_ordered = 0;
+
+                t += 1;
             }
         }
 
-        // If the last action was a type greater than the one we're trying to place, 
-        if self.steps[self.steps.len()-1].1 > action {
-            // Step forward to the next point in time.
-            state.ore += state.ore_bots;
-            state.clay += state.clay_bots;
-            state.obsidian += state.obsidian_bots;
-            state.geodes += state.geode_bots;
-
-            state.ore_bots += ordered_ore_bots;
-            ordered_ore_bots = 0;
-            state.clay_bots += ordered_clay_bots;
-            ordered_clay_bots = 0;
-            state.obsidian_bots += ordered_obsidian_bots;
-            ordered_obsidian_bots = 0;
-            state.geode_bots += ordered_geode_bots;
-            ordered_geode_bots = 0;
-
-            time += 1;
-        }
-
-        // Now, can we afford this action?  Step forward until we can
-        let affordable = 
-        while (time < TIME_PART1) && () {
-
-        }
-
-        None
-        // TODO
+        (geodes, geodes*1000 + obsidian*100 + clay*10 + ore)
     }
-    */
 
-    /*
-    // Try to schedule an action at the end of this solution.
-    // The return value consists of a flag and an Optional solution.
-    // If the flag is true, the current solution was found to be suboptimal
-    // and should be discarded.  The other component will be None in this case.
-    // The Option<Solution> will be None of the provided action can't be sheduled
-    // in the time frame.  If the action is feasible, the return value will be Some<s>
-    // where s is the current solution extended with the newly scheduled action.
-    fn try_action(&self, blueprint: Blueprint, action: &Action, time: usize) -> (bool, Option<Rc<Solution>>) {
-        // Set action time to None
-        let mut action_time: Option<usize> = None;
-        let mut states: [SimState; TIME_PART1+1];
-
-        // Step through time, updating resources
-        let mut action_id: usize = 0;
-        for t in 1..TIME_PART1 {
-            // copy resources left from previous time step.
-            states[t] = states[t-1];
-
-            let mut new_ore_bots = 0;
-            let mut new_clay_bots = 0;
-            let mut new_obsidian_bots = 0;
-            let mut new_geode_bots = 0;
-
-            // Start builds scheduled for now
-            while (action_id < self.steps.len()) && (self.steps[action_id].0 == t) {
-                match self.steps[action_id].1 {
-                    Action::Start_Ore(n) => {
-                        states[t].ore -= blueprint.ore_cost_ore*n;
-                        new_ore_bots += n;
-                    }
-                    Action::Start_Clay(n) => {
-                        states[t].ore -= blueprint.clay_cost_ore*n;
-                        new_clay_bots += n;
-                    }
-                    Action::Start_Obsidian(n) => {
-                        states[t].ore -= blueprint.obsidian_cost_ore*n;
-                        states[t].clay -= blueprint.obsidian_cost_clay*n;
-                        new_obsidian_bots += n;
-                    }
-                    Action::Start_Geode(n) => {
-                        states[t].ore -= blueprint.geode_cost_ore*n;
-                        states[t].obsidian -= blueprint.geode_cost_obsidian*n;
-                        new_geode_bots += n;
-                    }
-                }
-                action_id += 1;
-            }
-
-            // If we can afford the new action at this time...
-            //    And we couldn't afford it a moment ago,
-            //        Set action time to now
-            //    Else
-            //        (Action time is already set to an earlier time)
-            // Else
-            //    Set action time to none.
-
-            // Let the bots do their thing, collecting ore.
-            states[t].ore += states[t].ore_bots;
-            states[t].clay += states[t].clay_bots;
-            states[t].obsidian += states[t].obsidian_bots;
-            states[t].geodes += states[t].geode_bots;
-
-            // Add the newly built bots to employ next time
-            states[t].ore_bots += new_ore_bots;
-            states[t].clay_bots += new_clay_bots;
-            states[t].obsidian_bots += new_obsidian_bots;
-            states[t].geode_bots += new_geode_bots;
-        }     
-
-        // If action time is None, return None as the resulting solution.
-        // If action time is Some(t), t < latest action already performed, reject as suboptimal
-        // If action time is Some(t), t >= latest action already performed, append (t, action)
-        // If action was to start a geode bot, add geodes to the new solution's total.
-
-        // TODO
-        (false, None)
+    // Return the earliest time when the given action could be performed without
+    // interfering with the other actions already done.  (or None if not possible.)
+    pub fn next_action_time(&self, action: &Action) -> Option<usize> {
+        match action {
+            Action::Start_Clay => self.next_clay_bot,
+            Action::Start_Ore => self.next_ore_bot,
+            Action::Start_Obsidian => self.next_obsidian_bot,
+            Action::Start_Geode => self.next_geode_bot,
+        }
     }
-    */
 
 }
 
+#[derive(Debug)]
 struct Blueprint {
     id: usize,
     ore_cost_ore: usize,
@@ -326,28 +315,36 @@ impl Blueprint {
     fn quality_level(&self, time_limit: usize) -> usize {
         let mut max_geodes = 0;
 
-        let mut in_progress: PriorityQueue<Rc<Solution>, usize> = PriorityQueue::new();
-        let empty_solution = Solution::new();
+        println!("Assessing QL for {:?}", self);
+
+        let mut in_progress: PriorityQueue<Rc<ActionList>, usize> = PriorityQueue::new();
+        let empty_solution = ActionList::new();
         in_progress.push(Rc::new(empty_solution), 0);
-        let mut sim = Sim::new(self);
+        
 
         while in_progress.len() > 0 {
             // get candidate solution
             let candidate = in_progress.pop().unwrap().0;
 
-            // reset the simulator
-            sim.reset();
+            println!("Evaluating candidate: {:?}", candidate);
 
-            // Run the candidate solution through it.
-            let geodes = sim.run(candidate);
+            // Run the candidate solution through simulation
+            let mut sim = Sim::new(self);
+            let (geodes, score) = sim.run(&candidate);
+            println!("    Sim completed.  {:?} Geodes.", geodes);
             if geodes > max_geodes {
                 max_geodes = geodes;
+                println!("New best: {:?} Geodes via {:?}", geodes, candidate);
             }
 
-            for new_action in Action::Actions {
-                if let Some(t) = sim.next_action_time(new_action) {
+            for new_action in PART1_ACTIONS {
+                if let Some(t) = sim.next_action_time(&new_action) {
                     // extend the candidate with Action::Start_Clay(1) at time t
-                    in_progress.push(candidate.extend(new_action, t), geodes);
+                    // println!("Extending with {:?} at {:?}", new_action, t);
+                    in_progress.push(Rc::new(candidate.extend(t, new_action)), score);
+                }
+                else {
+                    // println!("Can't extend with {:?}", new_action);
                 }
             }
         }
@@ -415,7 +412,7 @@ mod tests {
     #[test]
     fn test_max_geodes() {
         let d = Day19::load("examples/day19_example1.txt");
-        assert_eq!(d.blueprints[0].quality_level(TIME_PART1), 9);
+        // assert_eq!(d.blueprints[0].quality_level(TIME_PART1), 9);
         assert_eq!(d.blueprints[1].quality_level(TIME_PART1), 24);
     }
 }
